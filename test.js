a5ff4e0cd4ef168427e8d354ba30f5d5bfb2a132
sumachanabc <nishida@uniaim.co.jp>
2024-08-07 00:34:18 +0900

    回答

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('node:assert');
+
+// テストの前に永続化されているファイルを消す
+const fs = require('node:fs');
+fs.unlinkSync('./tasks.json');
+const todo = require('./index.js');
 
 // add と list のテスト
 todo.add('ノートを買う');
